1912a66a3ca100c386bd5d521850b457d417bddd
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-09-20 09:12:07 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,12 +3,13 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(num1)
 })
 input.onButtonPressed(Button.AB, function () {
-	
+    basic.showNumber(num2)
 })
 input.onButtonPressed(Button.B, function () {
     num1 += -1
     basic.showNumber(num1)
 })
 let num1 = 0
+let num2 = 0
 basic.showIcon(IconNames.SmallHeart)
-num1 += randint(1, 25)
+num2 = num1
