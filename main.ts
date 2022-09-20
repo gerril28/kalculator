ac8fbba56566828bcae31f93672a3522dfe357ee
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-09-20 09:41:41 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,12 +2,17 @@ input.onButtonPressed(Button.A, function () {
     num1 += 1
     basic.showNumber(num1)
 })
-input.onButtonPressed(Button.AB, function () {
+input.onGesture(Gesture.TiltLeft, function () {
+    num1 += 1
     basic.showNumber(num2)
 })
 input.onButtonPressed(Button.B, function () {
     num1 += -1
     basic.showNumber(num1)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    num1 += -1
+    basic.showNumber(num2)
 })
 let num1 = 0
 let num2 = 0
